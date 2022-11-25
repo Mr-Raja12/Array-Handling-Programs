4ff3008b1d6934403914ea01224ff6117607cdc3
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-25 23:42:20 +0530

    Added a Program to Print Largest number in the Array

---
+++ LargestElement.java
@@ -0,0 +1,19 @@
+package com.bl.arrayproblems;
+
+public class LargestElement {
+	public static void main(String[] args) {
+
+        int [] a = new int[] {20, 30, 40, 80, 50};
+        int largest = a[0];
+
+        for (int i=0; i<a.length; i++) {
+
+            if (a[i] > largest) {
+                largest = a[i];
+            }
+        }
+        System.out.println("Largest element present in given Array: " + largest);
+    }
+}
+
+
